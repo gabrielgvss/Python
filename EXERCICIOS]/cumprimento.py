dff1426f38657b0ba4cd15f53ec8b8ca4b042381
Gabriel Vinícius <97963837+gabrielgvss@users.noreply.github.com>
2023-06-26 19:41:17 -0300

    Delete EXERCICIOS] directory

--- EXERCICIOS]/cumprimento.py
+++
@@ -1,9 +0,0 @@
-# Crie um programa que, dado o nome completo de alguém, cumprimente tal pessoa por seu primeiro nome
-
-def cumprimento(nome):
-    nome = nome.split()
-    return nome[0]
-
-nome = str(input("Digite seu nome: "))
-print("Olá, senhor(a) {}".format(cumprimento(nome)))
-
--- EXERCICIOS]/nomevertical.py
+++
@@ -1,5 +0,0 @@
-nome = str(input("Digite um nome:\n"))
-
-for i in range(len(nome)+1):
-    print(nome[0:i])
-    
